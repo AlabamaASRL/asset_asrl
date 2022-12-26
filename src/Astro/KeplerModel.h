33b6797a9b16880e49947e073cc30e231d9cb79b
jbpezent <40646929+jbpezent@users.noreply.github.com>
2022-12-26 15:48:02 -0600

    Split Kepler
    Seems to hangup on ASSET_Astro.cpp, splitting it and see if i see any improvements

--- src/Astro/KeplerModel.h
+++ src/Astro/KeplerModel.h
@@ -29,30 +29,7 @@ namespace ASSET {
             this->mu = mu;
         }
 
-        static void Build(py::module& m, const char* name) {
-            auto obj = py::class_<Kepler>(m, name).def(py::init<double>());
-            Base::DenseBaseBuild(obj);
-            obj.def("phase", [](const Kepler& od, TranscriptionModes Tmode) {
-                return std::make_shared<KeplerPhase>(od, Tmode);
-                });
-            obj.def("phase", [](const Kepler& od, TranscriptionModes Tmode,
-                        const std::vector<Eigen::VectorXd>& Traj, int numdef) {
-                    return std::make_shared<KeplerPhase>(od, Tmode, Traj, numdef);
-                });
-
-            obj.def("phase", [](const Kepler& od, std::string Tmode) {
-                return std::make_shared<KeplerPhase>(od, Tmode);
-                });
-            obj.def("phase", [](const Kepler& od, std::string Tmode,
-                const std::vector<Eigen::VectorXd>& Traj, int numdef) {
-                    return std::make_shared<KeplerPhase>(od, Tmode, Traj, numdef);
-                });
-
-            Integrator<Kepler>::BuildConstructors(obj);
-
-            
-
-        }
+        static void Build(py::module& m, const char* name);
 
     };
 
@@ -101,13 +78,7 @@ namespace ASSET {
     };
 
 
-    static void BuildKeplerMod(FunctionRegistry& reg, py::module& m) {
-        auto odemod = m.def_submodule("Kepler");
-        reg.template Build_Register<Kepler>(odemod, "ode");
-        reg.template Build_Register<Integrator<Kepler>>(odemod, "integrator");
-        reg.Build_Register<KeplerPropagator>(odemod, "KeplerPropagator");
-        KeplerPhase::Build(odemod);
-    }
+    void BuildKeplerMod(FunctionRegistry& reg, py::module& m);
 
 
 }
